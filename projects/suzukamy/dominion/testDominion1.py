7d4e7e34ad7156a5ab09717329a64acc509e9c0d
Amy Suzuki <amy.s.suzuki@gmail.com>
2020-01-17 18:14:29 -0800

    Introduce test pages and utility functions. Introduce bug in test pages

---
+++ projects/suzukamy/dominion/testDominion1.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 12:00:00 2020
+
+@author: amysuzuk
+"""
+import Dominion
+
+import testUtility as testUtility
+
+
+#Get player names
+player_names = ["Annie","*Ben","*Carla"]
+
+#number of curses and victory cards
+nV, nC = testUtility.curseAndVictoryNum(player_names)
+
+#Get Box
+box = testUtility.getBox(nV)
+
+#Get the supply order
+supply_order = testUtility.getSupplyOrder()
+
+#Get the supply
+supply = testUtility.getSupply(player_names, box, nV, nC)
+
+#Bug
+del supply["Province"]
+
+#initialize the trash
+trash = []
+
+#Costruct the Player objects
+players = testUtility.getPlayers(player_names)
+
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1
+    print("\r")
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
